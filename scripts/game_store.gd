6916062ad91b0d6fdef5b8b67f020a61a804959a
Viktor Login <batazor111@gmail.com>
2025-12-24 02:11:07 +0100

    refactoring

--- scripts/game_store.gd
+++ scripts/game_store.gd
@@ -3,22 +3,48 @@ extends Node
 signal factions_changed
 signal world_ready
 
-var factions: Dictionary = {}
+var factions: Dictionary[StringName, Variant] = {}
+
+var _world_ready_emitted: bool = false
 
 
 func _enter_tree() -> void:
-	# Register group early so other nodes can find the store during their _ready.
+	# Allow discovery before _ready
 	add_to_group("game_store")
 
 
-func register_faction(faction) -> void:
-	if faction == null or not ("id" in faction) or String(faction.id).is_empty():
+func register_faction(faction: Variant) -> void:
+	if faction == null:
 		return
-	factions[faction.id] = faction
+
+	if not ("id" in faction):
+		push_warning("Faction missing 'id'")
+		return
+
+	var id: StringName = StringName(faction.id)
+	if id == StringName():
+		push_warning("Faction has empty id")
+		return
+
+	if factions.has(id):
+		return  # already registered, ignore
+
+	factions[id] = faction
 	emit_signal("factions_changed")
-	if is_ready():
-		emit_signal("world_ready")
+
+	_try_emit_world_ready()
 
 
 func is_ready() -> bool:
 	return factions.size() > 0
+
+
+func _try_emit_world_ready() -> void:
+	if _world_ready_emitted:
+		return
+
+	if not is_ready():
+		return
+
+	_world_ready_emitted = true
+	emit_signal("world_ready")
